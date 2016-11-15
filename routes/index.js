17ac43009f139b74afb7ffbf68ec176cc815d0c9
Jeff <jeff_m_england@yahoo.com>
2016-11-15 20:33:29 +0000

    added functionality

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
 
+var months = ["january", "February", "march", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get("/:time", function(req, res) {
+    
+    function unixToNatural(unix) {
+        var date = new Date(unix * 1000);
+        var year = date.getFullYear();
+        var month = months[date.getMonth()];
+        var day = date.getDate();
+        
+        var result = month + " " + day + ", " + year;
+        return result;
+    };
+    
+    if(isNaN(req.params.time)) {
+        var natural = new Date(req.params.time);
+        if(!isNaN(natural)) {
+            var unix = natural / 1000;
+            var data = {unix: unix, natural: req.params.time};
+            res.json(data);
+        }
+        else {
+            res.json({unix: null, natural: null});
+        }
+    }
+    else {
+        var result = unixToNatural(req.params.time);
+        var data = {unix: req.params.time, natural: result};
+        res.json(data);
+    }
 });
 
 module.exports = router;
